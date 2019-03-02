f38058d29707da76c09ac413348e11d3f2aee0a6
rosepetal <gita.phelps@gcsu.edu>
2019-03-01 22:46:29 -0500

    added Fruits And Veggies Startup

---
+++ FruitsVegetableStrt.java
@@ -0,0 +1,22 @@
+<html>
+<head>
+</head>
+
+<body>
+  
+<h1> Food </h1>
+Food is any substance consumed to provide nutritional support for the body. 
+It is usually of plant or animal origin, and contains essential nutrients,
+such as fats, proteins, vitamins, or minerals.
+
+<p>Fruit
+</p>
+
+<p>Vegetables
+</p>
+  
+<p> Red Meat
+</p>
+
+<p> Poultry
+</html>
